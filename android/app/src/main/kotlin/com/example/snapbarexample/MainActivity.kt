f893d02458e74c79ff9e745dfa628f9896e63fb4
Sıla Kaçmaz <sila.kacmaz@obase.com>
2022-11-19 13:42:55 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/snapbarexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snapbarexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _snackBar = SnackBar(
+    content: Text('Flutter snackbar tutorial'),
+    backgroundColor: Colors.green,
+    duration: Duration(seconds: 10),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: showSnackBar, child: Text("Show SnackBar"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  showSnackBar() {
+    ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+  }
+}
